8d7b71abc067e48ae93f321d8402477160b12734
LuisAngelFnz <lfernandez@gmail.com>
2022-04-27 16:36:28 -0500

    Paso5: Agregando primera prueba de clase Reader y arreglando package.json

--- test/lib/Reader.test.js
+++ test/lib/Reader.test.js
@@ -1,5 +1,16 @@
-const Reader = require('../../lib/util/Reader');
+const Reader = require('../../lib/utils/Reader');
 
 describe('Test Suite for Reader', ()=>{
-    
+    test('1.- Method readJsonFile with not pass param and path null', ()=>{
+
+        let [success,data_explorers] = Reader.readJsonFile();
+
+        expect(success).toBe(false);
+        expect(data_explorers).toBe('Ruta invalida');
+
+        [success,data_explorers] = Reader.readJsonFile(null);
+
+        expect(success).toBe(false);
+        expect(data_explorers).toBe('Ruta invalida');
+    });
 });
