bf05bd90bf1442360c68c8defb453de937445e48
LuisAngelFnz <lfernandez@gmail.com>
2022-04-28 01:02:23 -0500

    Paso6: Termiando codigo de prueba unitaria 2

--- test/lib/Reader.test.js
+++ test/lib/Reader.test.js
@@ -1,7 +1,7 @@
 const Reader = require('../../lib/utils/Reader');
 
 describe('Test Suite for Reader', ()=>{
-    test('1.- Method readJsonFile with not pass param and path null', ()=>{
+    test('1.- Method readJsonFile path = undefined, null and not string', ()=>{
 
         let [success,data_explorers] = Reader.readJsonFile();
 
@@ -12,5 +12,18 @@ describe('Test Suite for Reader', ()=>{
 
         expect(success).toBe(false);
         expect(data_explorers).toBe('Ruta invalida');
+
+        [success,data_explorers] = Reader.readJsonFile(2);
+        expect(success).toBe(false);
+        expect(data_explorers).toBe('Ruta invalida');        
+    });
+
+    test('2.- Method readJsonFile path invalid', ()=>{
+        let [success, data_explorers] = Reader.readJsonFile(`/tmp/${new Date().valueOf()}`);
+
+        expect(success).toBe(false);
+
+        expect(data_explorers).toBe('Ruta no encontrada');
+
     });
 });
