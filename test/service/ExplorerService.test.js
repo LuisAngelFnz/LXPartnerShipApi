c55d62f8485b036d08f0b00169e886ec41171620
LuisAngelFnz <lfernandez@gmail.com>
2022-04-28 21:55:46 -0500

    Agregando prueba unitaria 2 de ExplorerService

--- test/service/ExplorerService.test.js
+++ test/service/ExplorerService.test.js
@@ -11,4 +11,17 @@ describe('Test Suite for class ExplorerService', ()=>{
         expect(success).toBe(true);
         expect(all_explorers).not.toBeUndefined();
     });
+
+    test('2.- Method getExplorers, with custom path', ()=>{
+        let path = `/tmp/${new Date().valueOf()}`
+        let [success,all_explorers] = ExplorerService.getExplorers(path);
+
+        expect(success).toBe(false);
+        expect(all_explorers).toBe('Ruta no encontrada');
+
+        path = 'explorers.json';
+        [success,all_explorers] = ExplorerService.getExplorers(path);
+        expect(success).toBe(true);
+        expect(all_explorers).not.toBeUndefined();
+    });
 });
