40ac3a4ce7dd58d01c58af741a8de416073647d1
LuisAngelFnz <lfernandez@gmail.com>
2022-05-01 01:54:24 -0500

    Adicionando prueba unitaria 2 para metodo applyValidationInExplorer

--- test/services/FizzbuzzService.test.js
+++ test/services/FizzbuzzService.test.js
@@ -7,11 +7,27 @@ describe('Test Suite for class FizzbuzzService',()=>{
         expect(result).toEqual({});
 
         result = FizzbuzzService.applyValidationInExplorer(undefined);
-        expect(result).toEqual({});        
-        
+        expect(result).toEqual({});
+        expect(result.trick).toBeUndefined();
+
     });
 
     test('2.- Method applyValidationInExplorer, pass params OK',()=>{
-        let all_explorers = []
+        let explorer = {'score':3};
+
+        let result = FizzbuzzService.applyValidationInExplorer(explorer);
+        expect(result.trick).toBe('FIZZ');
+
+        explorer = {'score':5};
+        result = FizzbuzzService.applyValidationInExplorer(explorer);
+        expect(result.trick).toBe('BUZZ');
+
+        explorer = {'score':15};
+        result = FizzbuzzService.applyValidationInExplorer(explorer);
+        expect(result.trick).toBe('FIZZBUZZ');
+
+        explorer = {'score':3};
+        result = FizzbuzzService.applyValidationInExplorer(explorer);
+        expect(result.trick).toBe(3);
     });
 });
