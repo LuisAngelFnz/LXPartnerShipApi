55a638976b547b41b189fa1631b2ce9046b3b00a
LuisAngelFnz <lfernandez@gmail.com>
2022-05-01 14:39:16 -0500

    Modificando función getExplorers de ExplorerService y sus pruebas unitarias

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -3,25 +3,20 @@ const ExplorerService = require('../../lib/services/ExplorerService');
 describe('Test Suite for class ExplorerService', ()=>{
 
     test('1.- Method getExplorers, pass param path, null and undefined(get with defaul path)',()=>{
-        let [success,all_explorers] = ExplorerService.getExplorers(null);
-        expect(success).toBe(true);
+        let all_explorers = ExplorerService.getExplorers(null);
         expect(all_explorers).not.toBeUndefined();
 
-        [success,all_explorers] = ExplorerService.getExplorers();
-        expect(success).toBe(true);
+        all_explorers = ExplorerService.getExplorers();
         expect(all_explorers).not.toBeUndefined();
     });
 
     test('2.- Method getExplorers, with custom path', ()=>{
         let path = `/tmp/${new Date().valueOf()}`
-        let [success,all_explorers] = ExplorerService.getExplorers(path);
-
-        expect(success).toBe(false);
-        expect(all_explorers).toBe('Ruta no encontrada');
+        let all_explorers = ExplorerService.getExplorers(path);
+        expect(all_explorers).toEqual([]);
 
         path = 'explorers.json';
-        [success,all_explorers] = ExplorerService.getExplorers(path);
-        expect(success).toBe(true);
+        all_explorers = ExplorerService.getExplorers(path);
         expect(all_explorers).not.toBeUndefined();
     });
 
