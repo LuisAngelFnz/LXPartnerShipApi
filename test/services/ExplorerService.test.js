4aaa79ec1c20160171f4ef64203c87dbe69eeee9
LuisAngelFnz <lfernandez@gmail.com>
2022-05-01 11:46:41 -0500

    Corrigiendo bug para prueba de GitHub Actions

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -8,7 +8,7 @@ describe('Test Suite for class ExplorerService', ()=>{
         expect(all_explorers).not.toBeUndefined();
 
         [success,all_explorers] = ExplorerService.getExplorers();
-        expect(success).toBe(false);
+        expect(success).toBe(true);
         expect(all_explorers).not.toBeUndefined();
     });
 
