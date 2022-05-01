fda7bb3fd6d589743545e9754536d28d82883687
LuisAngelFnz <lfernandez@gmail.com>
2022-05-01 01:46:14 -0500

    Redefiniendo metodo applyValidationInExplorer y sus respectivas pruebas

--- test/services/FizzbuzzService.test.js
+++ test/services/FizzbuzzService.test.js
@@ -4,10 +4,14 @@ describe('Test Suite for class FizzbuzzService',()=>{
     test('1.- Method applyValidationInExplorer, pass param null and undefined',()=>{
         
         let result = FizzbuzzService.applyValidationInExplorer(null);
-        expect(result).toEqual([]);
+        expect(result).toEqual({});
 
         result = FizzbuzzService.applyValidationInExplorer(undefined);
-        expect(result).toEqual([]);        
+        expect(result).toEqual({});        
         
+    });
+
+    test('2.- Method applyValidationInExplorer, pass params OK',()=>{
+        let all_explorers = []
     });
 });
