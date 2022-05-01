4b022e266943f6ff22569d50b5fd121dce61d6f5
LuisAngelFnz <lfernandez@gmail.com>
2022-05-01 14:48:50 -0500

    Añadiendo prueba unitaria 2, de método getExplorersByMission

--- test/constrollers/ExplorerController.test.js
+++ test/constrollers/ExplorerController.test.js
@@ -7,5 +7,22 @@ describe('Test suit for class ExplorerController',() => {
 
         result = ExplorerController.getExplorersByMission(undefined);
         expect(result).toEqual([]);
-    });  
+    });
+
+    test('2.- Method getExplorersByMission, pass params OK', () => {
+        const all_explorers = [
+            {
+                "githubUsername" : "ajolonauta2",
+                "mission"        : "java",
+            },
+            {
+                "githubUsername" : "ajolonauta1",
+                "mission"        : "node",
+            }
+        ];
+
+        let result = ExplorerController.getExplorersByMission(all_explorers, 'node');
+        expect(result.length).toBe(1);
+        expect(result[0].mission).toBe('node');
+    });
 });
